d522cf5647f566c8c46e85850db68e4288894e25
Ben <dejesus.ben@gmail.com>
2011-12-09 22:32:18 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eightbitbeer::Application.config.secret_token = '1ba06c241cf3db5bab88897e753c161d1b4bcd7237e6d0e8577bd2dbacfcbbec3f4bfd5decefb8d65e1f735824f552e512fb28d2ec45e1c1b4921fcf0903bd7b'
