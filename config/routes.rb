7572d80f3a0a7fc3c1e626e635fef83ba8f8ebcd
Ben <dejesus.ben@gmail.com>
2011-12-10 18:22:31 -0800

    Added more basic styles for tables, forms, and lists

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Eightbitbeer::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
+  resource :styleguide
 
   # See how all your routes lay out with "rake routes"
 
