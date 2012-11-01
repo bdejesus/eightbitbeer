d6eb5d8ca401ec32277e47ce6ee2c9486dd674f2
Ben <dejesus.ben@gmail.com>
2012-11-01 00:21:54 -0700

    Styled recipe pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def where_i_am
     return request.env['PATH_INFO']
   end
+
+  def signed_in?
+    return true if brewer_signed_in? || brewmeister_signed_in?
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :brewer
   has_many :steps
   has_many :ingredients
+
+  validates_presence_of :name, :style
+  validates_uniqueness_of :name
 end
