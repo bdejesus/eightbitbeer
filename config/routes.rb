3cb82f28b878ad536b16add1466c021f4808e34c
Ben <dejesus.ben@gmail.com>
2012-10-31 22:40:04 -0700

    converted application layout to haml; added login/logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Eightbitbeer::Application.routes.draw do
 
-  devise_for  :brewers,
-              :brewmeisters
+  devise_for  :brewers
+  devise_for  :brewmeisters, :path_names => { :sign_in => 'login', :sign_out => 'logout'}
               
   resources :ingredients,
             :steps,
