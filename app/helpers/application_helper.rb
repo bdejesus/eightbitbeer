5ecf475ca75fa1e0babf618a1341ba3ebb76c4ae
Ben <dejesus.ben@gmail.com>
2012-11-18 01:31:56 -0800

    Adds abv calc helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def link_button(title, link, options = [])
     link_to(title, link, options)
   end
+
+  def calculate_abv(og, fg)
+    return "#{number_with_precision(((og - fg) * 131.25), precision: 2)}% abv"
+  end
+
+  def calculate_alt_abv(og, fg)
+    return "#{number_with_precision(((76.08 * (og-fg) / (1.775 - og)) * (fg / 0.794)), precision: 2)}% abv"
+  end
 end
